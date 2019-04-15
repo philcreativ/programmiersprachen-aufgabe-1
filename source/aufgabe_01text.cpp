4a350265a135ef0af0103783e876e52466811d3e
philcreativ <philcodes01@gmail.com>
2019-04-15 09:53:04 +0200

    Aufagbe 1.4 b

--- source/aufgabe_01text.cpp
+++ source/aufgabe_01text.cpp
@@ -2,7 +2,19 @@
 
 
 
+int main () {
+int    a       = 9;
+bool   b       = false ;
+char   c       = ’a ’;
+double d       = 1.3;
+int const five = 5;
+double e       = a / five ;
 
+five = d ;
+
+return 0;
+
+}
 
 
 
