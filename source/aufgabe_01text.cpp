5e694b92eda53f2eb80c59fc554e7b653b267e6f
philcreativ <philcodes01@gmail.com>
2019-04-15 10:14:28 +0200

    Aufagbe 1.5 b

--- source/aufgabe_01text.cpp
+++ source/aufgabe_01text.cpp
@@ -1,16 +1,16 @@
-/*
 
-Aufgabe 1.4----------------
+
+//Aufgabe 1.4----------------
 
 int main () {
 int    a       = 9;
 bool   b       = false ;
 char   c       = ’a ’;
 double d       = 1.3;
-int const five = 5;         (const is a read-only state of an object during runtime)
-double e       = a / five ; (will not store the fractional part)
+int const five = 5;         //(const is a read-only state of an object during runtime)
+double e       = a / five ; //(will not store the fractional part)
 
-five = d ;  (Will give an error because of trying to change a const)
+five = d ;  //(Will give an error because of trying to change a const)
 
 return 0;
 
@@ -18,11 +18,18 @@ return 0;
 
 
 
-Aufgabe 1.5-----------------
+/*Aufgabe 1.5-----------------
 
 Definition 
 
-Intialisierung      
+Intialisierung   
+
+Initialization means telling compiler to allocate a memory to the initialized
+variable so that compiler can know that this variable is going to store some value.
+
+Example: */ extern int a
+
+
 Zuweisung           
 
 
